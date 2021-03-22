b03ff5b001a02a98034a67f3954a36e49d177ec1
Maya Flores Madere <maya@Mayas-Air.attlocal.net>
2021-03-22 15:15:03 -0500

    created autocompleter shell

---
+++ src/components/AutoCompleter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AutoCompleter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div> Hello World </div>
+        );
+    }
+}
+
+export default AutoCompleter;
